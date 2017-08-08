a334ccb6e1e4fc75ad902dc0de6d62d8ab79b3f7
Roaster Kay <overock@ex-em.com>
2017-08-08 09:55:06 +0900

    테스트용 entry와 모듈 entry 구

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    dev: './src/dev.js',
+    evui: './src/evui.js'
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
